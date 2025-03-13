6ac2df763379f07ac376758d1a12e57222b7cf5c
koen-dm <koen.de.bruijn@deliverymatch.nl>
2025-03-13 11:16:32 +0100

    feat: add implementation for fetching delivery options

--- src/Common/Pdk.php
+++ src/Common/Pdk.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace DeliveryMatch\Pdk\Common;
 
+use DateTimeImmutable;
+use DeliveryMatch\Pdk\Model\Carrier;
+use DeliveryMatch\Pdk\Model\DeliveryWindow;
+use DeliveryMatch\Pdk\Model\HomeDeliveryOption;
+use DeliveryMatch\Pdk\Model\PickupWindow;
+use DeliveryMatch\Pdk\Model\Price;
+use DeliveryMatch\Pdk\Model\ServiceLevel;
 use DeliveryMatch\Sdk\Api\Dto\Request\ShipmentRequest;
 use DeliveryMatch\Sdk\Client;
+use DeliveryMatch\Sdk\Exception\DeliveryMatchApiException;
 use DI\Container;
 use Http\Client\Exception;
 use JsonException;
@@ -35,7 +43,11 @@ class Pdk implements PdkInterface
     {
         $api = $this->api();
 
-        $response = $api->me()->isAuthenticated();
+        try {
+            $api->me()->isAuthenticated();
+        } catch (DeliveryMatchApiException) {
+            return false;
+        }
 
         return true;
     }
@@ -46,6 +58,8 @@ class Pdk implements PdkInterface
      */
     public function fetchShippingOptions(ShipmentRequest $request): array
     {
+        $format = "Y-m-d H:i";
+
         $api = $this->api();
 
         if ($request->hasIdentifier()) {
@@ -54,7 +68,59 @@ class Pdk implements PdkInterface
             $response = $api->shipments()->insert($request);
         }
 
-        return ["Mooise shipping option"];
+        $methods = [];
+        foreach ($response['shipmentMethods']['all'] as $method) {
+            $pickupDate = DateTimeImmutable::createFromFormat($format, "{$method['datePickup']} {$method['pickupTime']}");
+            $cutoffDate = DateTimeImmutable::createFromFormat($format, "{$method['datePickup']} {$method['cutoffTime']}");
+
+            if ($pickupDate === false || $cutoffDate === false) {
+                continue;
+            }
+
+            if (isset($method['dateDelivery'])) {
+                $deliveryDateFrom = DateTimeImmutable::createFromFormat($format, "{$method['dateDelivery']} {$method['timeFrom']}");
+                $deliveryDateTo = DateTimeImmutable::createFromFormat($format, "{$method['dateDelivery']} {$method['timeTo']}");
+            } else {
+                $deliveryDateFrom = false;
+                $deliveryDateTo = false;
+            }
+
+            $methods[] = new HomeDeliveryOption(
+                methodId: $method['methodId'],
+                checkId: $method['checkId'],
+                carrier: new Carrier(
+                    id: $method['carrier']['id'],
+                    name: $method['carrier']['name'],
+                    code: $method['carrier']['code'],
+                ),
+                serviceLevel: new ServiceLevel(
+                    id: $method['serviceLevel']['id'],
+                    name: $method['serviceLevel']['name'],
+                    description: $method['serviceLevel']['description'],
+                ),
+                configurationId: $method['configurationID'],
+                tariffId: $method['tariffID'],
+                routeId: $method['routeID'],
+                price: new Price(
+                    buy: $method['buy_price'],
+                    sell: $method['price'],
+                    currency: $method['currency']
+                ),
+                description: $method['description'],
+                title: $method['title'],
+                pickupWindow: new PickupWindow(
+                    pickupTime: $pickupDate,
+                    cutoffTime: $cutoffDate
+                ),
+                deliveryWindow: $deliveryDateTo && $deliveryDateFrom
+                    ? new DeliveryWindow(
+                        from: $deliveryDateFrom,
+                        to: $deliveryDateTo,
+                    ) : null
+            );
+        }
+
+        return $methods;
     }
 
 }
--- src/Factory/PdkFactory.php
+++ src/Factory/PdkFactory.php
@@ -8,24 +8,35 @@ use DeliveryMatch\Pdk\Common\Pdk;
 use DeliveryMatch\Pdk\Common\PdkInterface;
 use DeliveryMatch\Pdk\Facade\Facade;
 use DeliveryMatch\Sdk\Client;
+use DI\Container;
 use DI\ContainerBuilder;
 
 class PdkFactory
 {
     public static function create(int $clientId, string $apiKey, array ...$configs): PdkInterface
     {
-        $builder = new ContainerBuilder();
-        $builder->useAutowiring(true);
-        $builder->addDefinitions(
-            ["api" => new Client($apiKey, $clientId)],
-            ...$configs
-        );
-        $container = $builder->build();
+        $instance = new PdkFactory();
+        $container = $instance->setupContainer($clientId, $apiKey, ...$configs);
 
         $pdk = new Pdk($container);
 
         Facade::setPdkInstance($pdk);
 
         return $pdk;
+    }
+
+    private function setupContainer(int $clientId, string $apiKey, array ...$configs): Container
+    {
+        $builder = new ContainerBuilder();
+        $builder->useAutowiring(true);
+        $builder->addDefinitions(
+            [
+                "api" => new Client($apiKey, $clientId),
+                PdkInterface::class => \DI\autowire(Pdk::class)
+            ],
+            ...$configs
+        );
+
+        return $builder->build();
     }
 }
