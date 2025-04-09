7b924bf7c09423ef36eee5886a5291574c9626e7
koen-dm <koen.de.bruijn@deliverymatch.nl>
2025-04-09 16:16:34 +0200

    feat: migrate to psr logs

--- src/Facade/Logger.php
+++ src/Facade/Logger.php
@@ -7,10 +7,16 @@ namespace DeliveryMatch\Pdk\Facade;
 use DeliveryMatch\Pdk\Logger\LoggerInterface;
 
 /**
- * @method static void info(string $message)
- * @method static void warning(string $message)
- * @method static void error(string $message)
- * @see \DeliveryMatch\Pdk\Logger\LoggerInterface
+ * @method static void log($level, $message, array $context = [])
+ * @method static void alert($message, array $context = [])
+ * @method static void critical($message, array $context = [])
+ * @method static void debug($message, array $context = [])
+ * @method static void emergency($message, array $context = [])
+ * @method static void error($message, array $context = [])
+ * @method static void info($message, array $context = [])
+ * @method static void notice($message, array $context = [])
+ * @method static void warning($message, array $context = [])
+ * @see \Psr\Log\LoggerInterface
  */
 final class Logger extends Facade
 {
