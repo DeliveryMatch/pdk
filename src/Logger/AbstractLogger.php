c86f0c97dd0eec714bfa2728e213b0bb8f3c524d
koen-dm <koen.de.bruijn@deliverymatch.nl>
2025-04-10 10:39:16 +0200

    fix: function parameters

--- src/Logger/AbstractLogger.php
+++ src/Logger/AbstractLogger.php
@@ -6,53 +6,62 @@ namespace DeliveryMatch\Pdk\Logger;
 
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
-use Stringable;
 
 abstract class AbstractLogger implements LoggerInterface
 {
-    abstract public function log($level, string|Stringable $message, array $context = []): void;
+    /** @phpstan-ignore missingType.parameter */
+    abstract public function log($level, $message, array $context = []): void;
 
-    public function alert(string|Stringable $message, array $context = []): void
+    /** @phpstan-ignore missingType.parameter */
+    public function alert($message, array $context = []): void
     {
         $this->addEntry(LogLevel::ALERT, $message, $context);
     }
 
-    public function critical(string|Stringable $message, array $context = []): void
+    /** @phpstan-ignore missingType.parameter */
+    public function critical($message, array $context = []): void
     {
         $this->addEntry(LogLevel::CRITICAL, $message, $context);
     }
 
-    public function debug(string|Stringable $message, array $context = []): void
+    /** @phpstan-ignore missingType.parameter */
+    public function debug($message, array $context = []): void
     {
         $this->addEntry(LogLevel::DEBUG, $message, $context);
     }
 
-    public function emergency(string|Stringable $message, array $context = []): void
+    /** @phpstan-ignore missingType.parameter */
+    public function emergency($message, array $context = []): void
     {
         $this->addEntry(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function error(string|Stringable $message, array $context = []): void
+    /** @phpstan-ignore missingType.parameter */
+    public function error($message, array $context = []): void
     {
         $this->addEntry(LogLevel::ERROR, $message, $context);
     }
 
-    public function info(string|Stringable $message, array $context = []): void
+    /** @phpstan-ignore missingType.parameter */
+    public function info($message, array $context = []): void
     {
         $this->addEntry(LogLevel::INFO, $message, $context);
     }
 
-    public function notice(string|Stringable $message, array $context = []): void
+    /** @phpstan-ignore missingType.parameter */
+    public function notice($message, array $context = []): void
     {
         $this->addEntry(LogLevel::NOTICE, $message, $context);
     }
 
-    public function warning(string|Stringable $message, array $context = []): void
+    /** @phpstan-ignore missingType.parameter */
+    public function warning($message, array $context = []): void
     {
         $this->addEntry(LogLevel::WARNING, $message, $context);
     }
 
-    protected function addEntry(string $level, string|Stringable $message, array $context): void
+    /** @phpstan-ignore missingType.parameter */
+    protected function addEntry(string $level, $message, array $context): void
     {
         $message = "[DM PDK]: " . $message;
         $this->log($level, $message, $context);
